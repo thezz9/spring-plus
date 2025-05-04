76d96963b50c04dd6c8f824eacab95123bccacb0
thezz9 <thezz9@naver.com>
2025-05-05 04:41:06 +0900

    refactor: 1-3 JPQL 쿼리 조건 선택적으로 적용

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("""
+        SELECT t FROM Todo t
+        LEFT JOIN t.user
+        WHERE (:weather IS NULL OR t.weather = :weather)
+        AND (
+            (:startDateTime IS NULL AND :endDateTime IS NULL) OR
+            (:startDateTime IS NULL AND t.modifiedAt <= :endDateTime) OR
+            (:endDateTime IS NULL AND t.modifiedAt >= :startDateTime) OR
+            (t.modifiedAt BETWEEN :startDateTime AND :endDateTime)
+        )
+        ORDER BY t.modifiedAt DESC
+    """)
+    Page<Todo> findAllByDynamicCondition(
+            Pageable pageable,
+            @Param("weather") String weather,
+            @Param("startDateTime") LocalDateTime startDateTime,
+            @Param("endDateTime") LocalDateTime endDateTime
+    );
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -47,10 +51,13 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDate startDate, LocalDate endDate) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        LocalDateTime startDateTime = (startDate != null) ? startDate.atStartOfDay() : null;
+        LocalDateTime endDateTime = (endDate != null) ? endDate.atTime(LocalTime.MAX) : null;
+
+        Page<Todo> todos = todoRepository.findAllByDynamicCondition(pageable, weather, startDateTime, endDateTime);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
